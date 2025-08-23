da3bf09f65d29ce3f3f23d31c89d4c4b1272877b
Kherae <cwaldridge@live.com>
2025-08-23 12:32:08 -0700

    remove player only variant, fix up reg variant

--- src/kheTechMod/campaign/bountyrules/KheWarpedOmegaBountyCompleteScript.java
+++ src/kheTechMod/campaign/bountyrules/KheWarpedOmegaBountyCompleteScript.java
@@ -1,24 +1,44 @@
 package kheTechMod.campaign.bountyrules;
 
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.FleetDataAPI;
-import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.*;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
-//import com.fs.starfarer.api.characters.PersonAPI;
 import com.fs.starfarer.api.combat.ShipVariantAPI;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import com.fs.starfarer.api.fleet.FleetMemberType;
-import com.fs.starfarer.api.impl.campaign.rulecmd.AddShip;
-import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
+import com.fs.starfarer.api.impl.campaign.ids.MemFlags;
 import com.fs.starfarer.api.util.Misc;
+import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
+//import com.fs.starfarer.api.characters.PersonAPI;
+import com.fs.starfarer.api.impl.campaign.DerelictShipEntityPlugin;
+import com.fs.starfarer.api.impl.campaign.ids.Entities;
+import com.fs.starfarer.api.impl.campaign.ids.Factions;
+import com.fs.starfarer.api.impl.campaign.procgen.themes.BaseThemeGenerator;
+//import com.fs.starfarer.api.impl.campaign.procgen.themes.SalvageSpecialAssigner;
+//import com.fs.starfarer.api.impl.campaign.rulecmd.AddShip;
+import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.special.ShipRecoverySpecial;
+//import kheTechMod.combat.hullmods.KheUtilities;
+//import org.lwjgl.util.vector.Vector2f;
 //import kheTechMod.rules.KheWarpedCoreOfficerPlugin;
+//import kheTechMod.combat.hullmods.KheEmptyHullmod;
+//import org.magiclib.bounty.MagicBountyFleetEncounterContext;
+//import static org.magiclib.util.MagicCampaign.createDerelict;
+//import org.lazywizard.lazylib.MathUtils;
+//import org.lwjgl.util.vector.Vector2f;
 
 import java.util.List;
 import java.util.Map;
 
+
+import org.apache.log4j.Logger;
+
+@SuppressWarnings("unused")//this is called in rules_csv so IDE flags as unused
 public class KheWarpedOmegaBountyCompleteScript extends BaseCommandPlugin {
+    private final Logger log = Logger.getLogger(KheWarpedOmegaBountyCompleteScript.class);
     public final String myKey="$khewarpedomegasbountykey";
     @Override
+
+
     public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
         //this...should always be true?
         boolean success = (boolean) Global.getSector().getMemoryWithoutUpdate().get(myKey+"_succeeded");
@@ -28,27 +48,51 @@ public class KheWarpedOmegaBountyCompleteScript extends BaseCommandPlugin {
 //        "_failed"
 
         if(success){
-            FleetDataAPI playerFleet = Global.getSector().getPlayerFleet().getFleetData();
-            String variantIdT = "khe_tesseract_player_Hull";
-            String variantIdF = "khe_facet_player_Hull";
+            String variantIdT = "khe_tesseract_Hull";
+            String variantIdF = "khe_facet_Hull";
             ShipVariantAPI variantT = Global.getSettings().getVariant(variantIdT).clone();
             ShipVariantAPI variantF = Global.getSettings().getVariant(variantIdF).clone();
             variantT.setVariantDisplayName("Unattuned");
             variantF.setVariantDisplayName("Unattuned");
             FleetMemberAPI memberT = Global.getFactory().createFleetMember(FleetMemberType.SHIP, variantT);
             FleetMemberAPI memberF = Global.getFactory().createFleetMember(FleetMemberType.SHIP, variantF);
-            //three is too much with how powerful they are
-            //KheWarpedCoreOfficerPlugin offPlug = new KheWarpedCoreOfficerPlugin();
-            //PersonAPI newCaptainT = offPlug.createPerson("khe_warped_omega_core", "khewarpedomega", null);
-            //PersonAPI newCaptainF = offPlug.createPerson("khe_warped_omega_core", "khewarpedomega", null);
-            //memberF.setCaptain(newCaptainF);
-            //memberT.setCaptain(newCaptainT);
-            memberF.setShipName("Fragment-010F0001");
-            memberT.setShipName("Fragment-010F0002");
-            playerFleet.addFleetMember(memberT);
-            playerFleet.addFleetMember(memberF);
-            AddShip.addShipGainText(memberT, dialog.getTextPanel());
-            AddShip.addShipGainText(memberF, dialog.getTextPanel());
+
+            CampaignFleetAPI playerFleet = Global.getSector().getPlayerFleet();
+            FleetDataAPI playerFleetData = playerFleet.getFleetData();
+            LocationAPI playerloc = playerFleetData.getFleet().getContainingLocation();
+
+            CustomCampaignEntityAPI shipF = (CustomCampaignEntityAPI) BaseThemeGenerator.addSalvageEntity(
+                playerloc, Entities.WRECK, Factions.NEUTRAL, new DerelictShipEntityPlugin.DerelictShipData(
+                    new ShipRecoverySpecial.PerShipData(
+                        variantF, ShipRecoverySpecial.ShipCondition.WRECKED,"Fragment-010F0001","khewarpedomega",0
+                    ), false
+                )
+            );
+            shipF.getMemoryWithoutUpdate().set(MemFlags.ENTITY_MISSION_IMPORTANT, true);
+            CustomCampaignEntityAPI shipT = (CustomCampaignEntityAPI) BaseThemeGenerator.addSalvageEntity(
+                playerloc, Entities.WRECK, Factions.NEUTRAL,new DerelictShipEntityPlugin.DerelictShipData(
+                    new ShipRecoverySpecial.PerShipData(
+                        variantT, ShipRecoverySpecial.ShipCondition.WRECKED,"Fragment-010F0002","khewarpedomega",0
+                    ), false
+                )
+            );
+            shipF.getLocation().x = playerFleet.getLocation().x + (50f - (float) Math.random() * 100f);
+            shipT.getLocation().x = playerFleet.getLocation().x + (50f - (float) Math.random() * 100f);
+            shipF.getLocation().y = playerFleet.getLocation().y + (50f - (float) Math.random() * 100f);
+            shipT.getLocation().y = playerFleet.getLocation().y + (50f - (float) Math.random() * 100f);
+            shipF.setFacing((float)Math.random()*360f);
+            shipT.setFacing((float)Math.random()*360f);
+            Misc.makeImportant(shipF,"Reward fleet from a special bounty, the unyielding peanut.");
+            Misc.makeImportant(shipT,"Reward fleet from a special bounty, the headache inducing ghost.");
+            shipT.getMemoryWithoutUpdate().set(MemFlags.ENTITY_MISSION_IMPORTANT, true);
+            shipF.getMemoryWithoutUpdate().set(MemFlags.ENTITY_MISSION_IMPORTANT, true);
+            shipF.setDiscoverable(true);
+            shipT.setDiscoverable(true);
+
+//            SpecialItemData fart1=new SpecialItemData("item_modspec","khe_phase_anchor");
+//            SpecialItemData fart2=new SpecialItemData("item_modspec","khephasestasiso");
+//            shipT.getCargo().addSpecial(fart1,1000);
+//            shipT.getCargo().addSpecial(fart2,1000);
         }
 
         return success;
--- src/kheTechMod/campaign/bountyrules/KheWarpedOmegaBountyStartScript.java
+++ src/kheTechMod/campaign/bountyrules/KheWarpedOmegaBountyStartScript.java
@@ -6,9 +6,13 @@ import com.fs.starfarer.api.campaign.FactionAPI;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
 import com.fs.starfarer.api.characters.PersonAPI;
+import com.fs.starfarer.api.combat.ShipHullSpecAPI;
+import com.fs.starfarer.api.combat.ShipVariantAPI;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import com.fs.starfarer.api.impl.campaign.ids.MemFlags;
+import com.fs.starfarer.api.impl.campaign.ids.Tags;
 import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
+import com.fs.starfarer.api.loading.VariantSource;
 import com.fs.starfarer.api.util.Misc;
 import kheTechMod.campaign.coreofficerplugins.KheWarpedCoreOfficerPlugin;
 
@@ -24,6 +28,16 @@ import java.util.Map;
 @SuppressWarnings("unused")//this is called in rules_csv so IDE flags as unused
 public class KheWarpedOmegaBountyStartScript extends BaseCommandPlugin {
     private static final Logger log = Logger.getLogger(KheWarpedOmegaBountyStartScript.class);
+    public static void fixVariant(FleetMemberAPI member) {
+        if (member.getVariant().getSource() != VariantSource.REFIT) {
+            ShipVariantAPI variant = member.getVariant().clone();
+            variant.setOriginalVariant(null);
+            variant.setHullVariantId(Misc.genUID());
+            variant.setSource(VariantSource.REFIT);
+            member.setVariant(variant, false, true);
+        }
+        member.updateStats();
+    }
     @Override
     public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
         String your_bounty_id = "khewarpedomegas";
@@ -56,7 +70,13 @@ public class KheWarpedOmegaBountyStartScript extends BaseCommandPlugin {
             KheWarpedCoreOfficerPlugin offPlug = new KheWarpedCoreOfficerPlugin();
             PersonAPI newCaptain = offPlug.createPerson("khe_warped_omega_core", "khewarpedomega", null);
             member.setCaptain(newCaptain);
+            fixVariant(member);
             member.getRepairTracker().setCR(member.getRepairTracker().getMaxCR());
+            member.getVariant().removeTag(Tags.VARIANT_ALWAYS_RECOVERABLE);
+            member.getVariant().removeTag(Tags.AUTOMATED_RECOVERABLE);
+            member.getVariant().getHints().add(ShipHullSpecAPI.ShipTypeHints.UNBOARDABLE);
+            member.getVariant().addTag(Tags.VARIANT_UNBOARDABLE);
+            member.getVariant().addTag(Tags.VARIANT_UNRESTORABLE);
         }
         bountyFleet.getMemoryWithoutUpdate().set(MemFlags.FLEET_FIGHT_TO_THE_LAST, true);
         return true;
