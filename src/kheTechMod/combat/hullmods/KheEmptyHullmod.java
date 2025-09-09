abb92247faa4e69f41470a3b7c19af32f93be3d0
Kherae <cwaldridge@live.com>
2025-09-08 22:53:33 -0700

    documentation for later reminder

--- src/kheTechMod/combat/hullmods/KheEmptyHullmod.java
+++ src/kheTechMod/combat/hullmods/KheEmptyHullmod.java
@@ -9,4 +9,7 @@ import com.fs.starfarer.api.combat.BaseHullMod;
 @SuppressWarnings("unused")
 public class KheEmptyHullmod extends BaseHullMod {
     private final Logger log = Logger.getLogger(KheEmptyHullmod.class);
+//    from basehullmod, about the class as a whole:
+//    Note: the effect class is instantiated once per application session. //remember this part, dingus. EFFECT CLASS IS INSTANCIATED ONCE.
+//    Storing campaign data in data members of an implementing class is a bad idea (will likely cause memory leaks), use SectorAPI.getPersistentData() instead.
 }
